e177c386ccd7c7e6e10738181b913bb121788ae3
Ming-Lung <danny83313@gmail.com>
2018-11-26 17:25:58 +0800

    基本MAVLINK測試程式建置

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GCS
 {
-    partial class Form1
+    partial class GCS
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -29,12 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.CMB_comport = new System.Windows.Forms.ComboBox();
+            this.CMB_baudrate = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_armdisarm = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // CMB_comport
+            // 
+            this.CMB_comport.FormattingEnabled = true;
+            this.CMB_comport.Location = new System.Drawing.Point(12, 12);
+            this.CMB_comport.Name = "CMB_comport";
+            this.CMB_comport.Size = new System.Drawing.Size(121, 20);
+            this.CMB_comport.TabIndex = 0;
+            this.CMB_comport.Click += new System.EventHandler(this.CMB_comport_Click);
+            // 
+            // CMB_baudrate
+            // 
+            this.CMB_baudrate.FormattingEnabled = true;
+            this.CMB_baudrate.Items.AddRange(new object[] {
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200"});
+            this.CMB_baudrate.Location = new System.Drawing.Point(154, 12);
+            this.CMB_baudrate.Name = "CMB_baudrate";
+            this.CMB_baudrate.Size = new System.Drawing.Size(121, 20);
+            this.CMB_baudrate.TabIndex = 1;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(313, 13);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // button_armdisarm
+            // 
+            this.button_armdisarm.Location = new System.Drawing.Point(441, 13);
+            this.button_armdisarm.Name = "button_armdisarm";
+            this.button_armdisarm.Size = new System.Drawing.Size(75, 23);
+            this.button_armdisarm.TabIndex = 3;
+            this.button_armdisarm.Text = "Arm/Disarm";
+            this.button_armdisarm.UseVisualStyleBackColor = true;
+            this.button_armdisarm.Click += new System.EventHandler(this.button_armdisarm_Click);
+            // 
+            // GCS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(687, 344);
+            this.Controls.Add(this.button_armdisarm);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.CMB_baudrate);
+            this.Controls.Add(this.CMB_comport);
+            this.Name = "GCS";
+            this.Text = "GCS";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox CMB_comport;
+        private System.Windows.Forms.ComboBox CMB_baudrate;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_armdisarm;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
