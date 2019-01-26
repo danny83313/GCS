41c681f0e55972a5e868e0a12826f2db9c3d7eb5
Ming-Lung <danny83313@gmail.com>
2019-01-26 11:56:05 +0800

    test

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace GCS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GCS());
+            ////1
         }
     }
 }
